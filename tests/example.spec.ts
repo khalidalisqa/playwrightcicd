df2d9e813888530db519063b6ac98fd69b866116
khalidalisqa <khalidalisqa@gmail.com>
2025-11-23 20:28:59 +0500

    rerun test fail test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@
 import { test, expect } from "@playwright/test";
 
 test("Google Search", async ({ page }) => {
-  await page.goto("https://www.google.com/");
+  await page.goto("https://www.ggle.com/");
   await page.fill("[name='q']", "LambdaTest Playwright");
   await page.keyboard.press("Enter");
   await page.waitForTimeout(3000);
